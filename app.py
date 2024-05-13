7c9be3e141c4b433b463dde170e224632251d4f2
witusj <wfth69@gmail.com>
2024-05-13 13:10:14 +0200

    chore: Remove unused pygame code and dependency

--- app.py
+++ app.py
@@ -5,17 +5,7 @@ try:
   from llama_index import VectorStoreIndex, ServiceContext, Document, SimpleDirectoryReader
 except ImportError:
   from llama_index.core import VectorStoreIndex, ServiceContext, Document, SimpleDirectoryReader
-import pygame
 
-# Initialize the pygame mixer
-pygame.mixer.init()
-
-def play_music(music_file):
-    try:
-        pygame.mixer.music.load(music_file)
-        pygame.mixer.music.play(loops=-1)  # Play the music file in a loop
-    except Exception as e:
-        print(f"Failed to play music: {e}")
 
 st.set_page_config(page_title="Chat with MPAC", page_icon="speak.png", layout="centered", initial_sidebar_state="auto", menu_items=None)
 openai.api_key = st.secrets.openai_key
@@ -28,14 +18,12 @@ if "messages" not in st.session_state.keys(): # Initialize the chat messages his
 
 @st.cache_resource(show_spinner=False)
 def load_data():
-    play_music('./music/wait.wav')
     with st.spinner(text="We are gathering the necesssary information – Please hang on! This can take up to a minute."):
         # Call to play music
         reader = SimpleDirectoryReader(input_dir="./data/manuals/", recursive=True)
         docs = reader.load_data()
         service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="U bent een expert op het gebied van aansluiting, levering en opwekking van energie voor een energiemaatschappij. Uw taak is om technische vragen te beantwoorden. Ga ervan uit dat alle vragen betrekking hebben op deze diensten. Houd uw antwoorden technisch en feitelijk; verzin geen kenmerken die er niet zijn."))
         index = VectorStoreIndex.from_documents(docs, service_context=service_context)
-        pygame.mixer.music.stop()  # Stop the music after loading is complete
         return index
     
 index = load_data()
