4500e532b36b740639a164e33070b15e347b36ca
witusj <wfth69@gmail.com>
2024-05-13 14:13:41 +0200

    chore: Update chat engine initialization with memory and context prompt

--- app.py
+++ app.py
@@ -5,7 +5,9 @@ try:
   from llama_index import VectorStoreIndex, ServiceContext, Document, SimpleDirectoryReader
 except ImportError:
   from llama_index.core import VectorStoreIndex, ServiceContext, Document, SimpleDirectoryReader
+from llama_index.core.memory import ChatMemoryBuffer
 
+memory = ChatMemoryBuffer.from_defaults(token_limit=3900)
 
 st.set_page_config(page_title="Chat with MPAC", page_icon="speak.png", layout="centered", initial_sidebar_state="auto", menu_items=None)
 openai.api_key = st.secrets.openai_key
@@ -22,14 +24,28 @@ def load_data():
         # Call to play music
         reader = SimpleDirectoryReader(input_dir="./data/manuals/", recursive=True)
         docs = reader.load_data()
-        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="U bent een expert op het gebied van aansluiting, levering en opwekking van energie voor een energiemaatschappij. Uw taak is om technische vragen te beantwoorden. Ga ervan uit dat alle vragen betrekking hebben op deze diensten. Houd uw antwoorden technisch en feitelijk; verzin geen kenmerken die er niet zijn."))
+        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0, system_prompt="You are an expert in the field of MPAC machines. Your task is to answer technical questions. Assume that all questions relate to the machines supplied by MPAC. Keep your answers technical and factual; do not invent features that do not exist."))
         index = VectorStoreIndex.from_documents(docs, service_context=service_context)
         return index
     
 index = load_data()
 
 if "chat_engine" not in st.session_state.keys(): # Initialize the chat engine
-        st.session_state.chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
+    st.session_state.chat_engine = index.as_chat_engine(
+    chat_mode="condense_plus_context",
+    memory=memory,
+    llm=OpenAI(model="gpt-3.5-turbo", temperature=0),
+    context_prompt=(
+        "You are an expert in the field of MPAC machines."
+        "Your task is to answer technical questions. Assume that all questions relate to the machines supplied by MPAC."
+        "Keep your answers technical and factual; do not invent features that do not exist."
+        "Here are the relevant documents for the context:\n"
+        "{context_str}"
+        "\nInstruction: Use the previous chat history, or the context above, to interact and help the user."
+    ),
+    verbose=False,
+)
+        # st.session_state.chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
 
 if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
